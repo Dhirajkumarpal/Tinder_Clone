899b2f47c2085038d555e817456bf1c05bda3ea0
dhirajkumar <dhirajp100199@gmail.com>
2020-11-15 14:52:17 +0530

    dd

--- my-app/src/TinderCards.js
+++ my-app/src/TinderCards.js
@@ -1,25 +1,54 @@
 import React, { useState } from "react"
 import './TinderCards.css';
-
+import TinderCard from "react-tinder-card";
 function TinderCards() {
   const [people,setPeople]  = useState([
       {
           name:"dhirAJ PAL",
-          url:"",
+          url:"C:/TinderClone/Tinder_Clone/my-app/src/24446-2-shrek-file-thumb.png",
       },
-            {
+      {
           name:"ram PAL",
-          url:"",
+          url:"https://images.app.goo.gl/nd3S44b66GsUuwe47",
       },
+      {
+          name:"shyam PAL",
+          url:"https://images.app.goo.gl/nd3S44b66GsUuwe47",
+      },
+  
   ])  
+  const swiped=(direction,nameToDelete)=>{
+    console.log("removing"+nameToDelete);
+    
+  }
+  const outOfFrame=(name)=>{
+    console.log(name+"left the screen");
+  }
+
   return (
     <div className="tinderCards">
+      <div className="tinderCards__cardContainer">
           {
               people.map((person)=>(
-              <h1>{person.name}</h1>
+              
+              <TinderCard
+              className="swipe"
+              key={person.name}
+              preventSwipe={["up","down"]}
+              onSwipe={(dir)=>swiped(dir,person.name)}
+              onCardLeftScreen={()=>outOfFrame(person.name)}
+              >
+              <div style={{backgroundImage:`url(${person.url})`}}
+              className="card"
+              >
+              <h3>{person.name}</h3>
+              
+              </div>  
+               
+              </TinderCard>
               ))
           }
-    
+      </div>
     </div>
     
     //header
